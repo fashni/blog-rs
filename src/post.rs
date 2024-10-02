c822c18fd944c6d0338d379e1530c760b1ad9bf5
fashni <fashni@tuta.io>
2024-10-02 13:48:10 +0700

    add path increment for duplicates titles, handle incorrect post format

--- src/post.rs
+++ src/post.rs
@@ -35,24 +35,38 @@ pub struct Post {
 impl Post {
   fn load_posts() -> Vec<Post> {
     let mut posts: Vec<Post> = Vec::new();
+    let mut paths = HashMap::new();
+
     let posts_dir = fs::read_dir("posts").expect("Failed to read posts dir");
     for entry in posts_dir {
       let path = entry.unwrap().path();
       let ext = path.extension().and_then(|ext| ext.to_str());
       if path.is_file() && ext == Some("md") {
-        let post = Post::from_markdown(path.to_str().unwrap());
-        posts.push(post);
+        if let Some(post) = Post::from_markdown(path.to_str().unwrap()){
+          posts.push(post);
+        }
       }
     }
+
     posts.sort_by(|a, b| b.created_time.cmp(&a.created_time));
+    for post in posts.iter_mut().rev() {
+      if let Some(count) = paths.get_mut(&post.path) {
+        *count += 1;
+        post.path = format!("{}-{}", post.path, count);
+      } else {
+        paths.insert(post.path.clone(), 0);
+      }
+    }
+
     posts
   }
 
-  pub fn from_markdown(file_name: &str) -> Self {
+  pub fn from_markdown(file_name: &str) -> Option<Self> {
     let raw_content = fs::read_to_string(file_name).unwrap();
     let parts: Vec<&str>= raw_content.splitn(3, "---\n").collect();
     if parts.len() != 3 {
-      panic!("The file {} is not in the correct format", file_name);
+      println!("The file {} is not in the correct format, skipping", file_name);
+      return None;
     }
 
     let (title, created_time, modified_time, published) = Self::parse_metadata(parts[1]);
@@ -61,7 +75,7 @@ impl Post {
       .filter(|c| c.is_alphanumeric() || *c == ' ' || *c == '-')
       .collect::<String>().to_lowercase().replace(' ', "-");
 
-    Post {
+    Some(Post {
       created_time,
       modified_time,
       title,
@@ -69,7 +83,7 @@ impl Post {
       content,
       path,
       published,
-    }
+    })
   }
 
   pub fn format_time(&self, time_type: TimeType) -> String {
@@ -109,14 +123,16 @@ impl Post {
       }
     }).collect();
 
-    let title = metadata.get("title").expect("Missing title").to_string();
-    let published = metadata.get("published").map(|p| p == "true").unwrap_or(false);
+    let title = metadata.get("title").filter(
+      |t| !t.is_empty()
+    ).cloned().unwrap_or_else(|| "Untitled".to_string());
     let created_time = metadata.get("created_time").and_then(
       |time_str| OffsetDateTime::parse(time_str, &format_description::well_known::Rfc3339).ok()
-    ).unwrap_or(OffsetDateTime::now_utc());
+    ).unwrap_or_else(|| OffsetDateTime::now_utc());
     let modified_time = metadata.get("modified_time").and_then(
       |time_str| OffsetDateTime::parse(time_str, &format_description::well_known::Rfc3339).ok()
-    ).unwrap_or(OffsetDateTime::now_utc());
+    ).unwrap_or_else(|| OffsetDateTime::now_utc());
+    let published = metadata.get("published").map(|p| p == "true").unwrap_or(false);
 
     (title, created_time, modified_time, published)
   }
